5c56943e7fbc5584bca94fd8792962232f7be5c6
PentestPriz <hniktozzz@gmail.com>
2025-05-08 13:54:24 +0200

    Mensajes de prueba para la documentación (no me es posible generarla con sphynx).

--- jugador.py
+++ jugador.py
@@ -1,19 +1,41 @@
-#Clase Jugador del proyecto Digipymon
-#La clase se encargará de la gestión de jugadores y la asociación con sus digipymons
-
 class Jugador:
-    #Constructor por parámetros de la clase jugador.
+    """
+    Clase Jugador del proyecto Digipymon.
+    La clase se encargará de la gestión de jugadores y la asociación con sus digipymons.
+    """
     def __init__(self, nombre):
+        """
+        Constructor por parámetros de la clase Digipymon.
+        
+        Parameters:
+        self: Referencia a los atributos propios.
+        nombre (str): Cadena de caracteres que representa el nombre del jugador
+        """
         self.nombre = nombre
         lista_digpymon = []
         cantidad_digipymon = 0
         digicoins = 10
 
+
     def añadir_digipymon(self, digipymon):
+        """
+        Método para añadir digipymons al jugador, sumándolos a su lista.
+
+        Parameters:
+        self: Referencia a los atributos propios.
+        digipymon (Digipymon): Objeto de la clase Digipymon que añadiremos al jugador.
+        """
         self.lista_digipymon.apend(digipymon)
         cantidad_digipymon = cantidad_digipymon + 1
 
     def consultar_digipymon(self, digipymon):
+        """
+        Método para consultar la lista de digipymons, recorriéndola e imprimiñendola.
+
+        Parameters:
+        self: Referencia a los atributos propios.
+        digipymon (Digipymon): Objeto de la clase Digipymon que añadiremos al jugador
+        """
         print("Estos son todos tus digipymon.")
         for iterador in digipymon:
             print("- " + iterador)
