945edcaa7774f561a64738b2cb600ee388916ec6
AlexSMedac <asm0048@alu.medac.es>
2025-05-12 15:15:57 +0200

    Avance

--- enemigo.py
+++ enemigo.py
@@ -1,3 +1,9 @@
 class enemigo:
     def __init__(self, nombre):
         self.nombre = (nombre)
+        lista_digipymon = []
+        cantidad_digipymon = 0
+    
+    def añadir_digipymon(self, digipymon):
+        self.lista.digipymons(digipymon)
+        self.cantidad.digipymon = digipymon + 1
