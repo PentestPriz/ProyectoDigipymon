1b3bbd7fc557700a57b71052c987db6ec80e7f31
AlexSMedac <asm0048@alu.medac.es>
2025-05-12 16:01:05 +0200

    Documentacion1

--- enemigo.py
+++ enemigo.py
@@ -1,9 +1,27 @@
 class enemigo:
+    """
+    Clase Enemigo del proyecto Digipymon.
+    La clase se encargará de la gestión de enemigos  y la asociación con sus digipymons.
+    """
     def __init__(self, nombre):
+        """
+        Constructor por parámetros de la clase Digipymon.
+        
+        Args:
+            self: Referencia a los atributos propios.
+            nombre (str): Cadena de caracteres que representa el nombre del enemigo
+        """
         self.nombre = (nombre)
         lista_digipymon = []
         cantidad_digipymon = 0
     
     def añadir_digipymon(self, digipymon):
+        """
+        Método para añadir digipymons al enemigo, sumándolos a su lista.
+
+        Args:
+            self: Referencia a los atributos propios.
+            digipymon (Digipymon): Objeto de la clase Digipymon que añadiremos al enemigo.
+        """
         self.lista.digipymons(digipymon)
         self.cantidad.digipymon = digipymon + 1
