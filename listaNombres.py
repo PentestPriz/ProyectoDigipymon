1291119ab47aee3cb2aac40f7012e8981487be5e
AlexSMedac <asm0048@alu.medac.es>
2025-05-13 15:25:39 +0200

    Inicio primera lista

---
+++ listaNombres.py
@@ -0,0 +1,8 @@
+import random
+
+class clase_ListaNombres:
+    def __init__ (self, nombres_digipymon, nombres_entrenadores):
+        self.nombres_digipymon = ["Bugnix", "Larvok", "Mandiblash", "Zappinch", "Spirafly", "Chitinox", "Drilburron", "Glowmite", 
+                                  "Venogrit", "Scorvex", "Fungnash", "Toxibuzz", "Webdrill", "Creepuff", "Nettling", "Slinklaw",
+                                  "Moltwist", "Stingrowl", "Pupagoon", "Silkobrax"]
+
