4ec0214b2d8bc3ea5fb39dddc21dcb4267798b15
AlexSMedac <asm0048@alu.medac.es>
2025-05-13 17:45:34 +0200

    Corrección constructor por defecto.

--- listaNombres.py
+++ listaNombres.py
@@ -5,20 +5,16 @@ class clase_ListaNombres:
     Clase ListaNombres del Proyecto Digipymon.
     Esta clase nos servirá para gestionar las listas de Digipymons y de entrenadores enemigos.
     """
-    def __init__ (self, nombres_digipymon, nombres_entrenadores):
+    def __init__ (self):
         """
-         Constructor por parámetros de la clase ListaNombres.
+        Constructor por defecto de la clase ListaNombres
 
-         Args:
-            nombres_digipymon (str): Cadena de texto donde se muestran los nombres de los diferentes 20 digipymons del juego.
-            nombres_entrenadores (str): Cadena de texto donde se muestran los nombres de los diferentes 20 entrenadores del juego.
+        Args:
+            self: Referencia a los atributos propios
         """
-        self.nombres_digipymon = ["Bugnix", "Larvok", "Mandiblash", "Zappinch", "Spirafly", "Chitinox", "Drilburron", "Glowmite", 
-                                  "Venogrit", "Scorvex", "Fungnash", "Toxibuzz", "Webdrill", "Creepuff", "Nettling", "Slinklaw",
-                                  "Moltwist", "Stingrowl", "Pupagoon", "Silkobrax"]
+        self.nombres_digipymon = ["Bugnix", "Larvok", "Mandiblash", "Zappinch", "Spirafly", "Chitinox", "Drilburron", "Glowmite", "Venogrit", "Scorvex", "Fungnash", "Toxibuzz", "Webdrill", "Creepuff", "Nettling", "Slinklaw", "Moltwist", "Stingrowl", "Pupagoon", "Silkobrax"]
 
-        self.nombres_entrenadores = ["Kaelor", "Tamina", "Rudrek", "Vionne", "Zark", "Elira", "Bront", "Mysha", "Kazen", "Thorne", 
-                                     "Luneth", "Draxil", "Selka", "Jurok", "Amari", "Fenrik", "Vessa", "Torak", "Nimelle", "Quorin"]    
+        self.nombres_entrenadores = ["Kaelor", "Tamina", "Rudrek", "Vionne", "Zark", "Elira", "Bront", "Mysha", "Kazen", "Thorne", "Luneth", "Draxil", "Selka", "Jurok", "Amari", "Fenrik", "Vessa", "Torak", "Nimelle", "Quorin"]    
         
     def obtener_nombredigipymon(self):
         """
