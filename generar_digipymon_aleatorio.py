bb57765881d97ccfbb040c7ce67133233b1dc2e3
AlexSMedac <asm0048@alu.medac.es>
2025-05-14 16:22:41 +0200

    Documentacion1

--- generar_digipymon_aleatorio.py
+++ generar_digipymon_aleatorio.py
@@ -1,6 +1,13 @@
 def generar_digipymon_aleatorio():
     """
     Función que nos permitirá generar un digipymon aleatorio.
+    
+    Args:
+        vida (int): Genera puntos de vida aleatorios entre 10 y 20.
+        ataque (int): Genera puntos de ataque aleatorios entre 1 y 10.
+        nivel (int): Genera un nivel aleatorio entre 1 y 3.
+        tipo (str): Genera un tipo de digipymon entre fuego, agua y planta.
+        nombre (str): Genera un nombre aleatorio de digipymon de la lista de digipymons.
     """
     vida = random.randint(10, 20)
     ataque = random.randint(1, 10)
