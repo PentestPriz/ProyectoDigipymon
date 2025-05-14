778687fd962761d9c37d2e68013181ec40b417bb
AlexSMedac <asm0048@alu.medac.es>
2025-05-14 19:39:07 +0200

    Ultimo avance de codigo

--- generar_digipymon_aleatorio.py
+++ generar_digipymon_aleatorio.py
@@ -13,7 +13,7 @@ def generar_digipymon_aleatorio():
     ataque = random.randint(1, 10)
     nivel = random.randint(1,3)
     tipo = random.choice("fuego", "agua", "planta")
-    nombre = lista.obtener_nombre_digipymon()
+    nombre = ListaNombres.obtener_nombredigipymon()
     
-    return digipymon(nombre, tipo, nivel, vida, ataque)
- 
+    digipymonObjeto = Digipymon(nombre, tipo, nivel, vida, ataque)
+    return digipymonObjeto
