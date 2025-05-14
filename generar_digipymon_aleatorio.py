520e303c88d6ede3cbb35b9705648f0694703b0b
AlexSMedac <asm0048@alu.medac.es>
2025-05-14 16:15:00 +0200

    Creación atributos

--- generar_digipymon_aleatorio.py
+++ generar_digipymon_aleatorio.py
@@ -2,3 +2,9 @@ def generar_digipymon_aleatorio():
     """
     Función que nos permitirá generar un digipymon aleatorio.
     """
+    vida = random.randint(10, 20)
+    ataque = random.randint(1, 10)
+    nivel = random.randint(1,3)
+    tipo = random.choice("fuego", "agua", "planta")
+    nombre = lista.obtener_nombre_digipymon()
+ 
