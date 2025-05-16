99ae99ad4cc8d570baacf34d6d56ec46dd7144e0
AlexSMedac <asm0048@alu.medac.es>
2025-05-16 16:06:56 +0200

    Correcciones menores

--- buscar_digipymon.py
+++ buscar_digipymon.py
@@ -2,5 +2,4 @@ def buscar_digipymon_aleatorio(jugador, inventario):
     digipymonObjeto = generar_digipymon_aleatorio()
     print(f"{digipymonObjeto}")
     probabilidad = 100 - (digipymonObjeto.nivel * 10)
-    print(probabilidad)
-    
+    print(f"{probabilidad}")
--- main.py
+++ main.py
@@ -6,7 +6,28 @@ from digipymon import Digipymon
 from enemigo import Enemigo
 from inventario import Inventario
 from jugador import Jugador
+from lista_nombres import ListaNombres
 import random
+
+def generar_digipymon_aleatorio():
+    """
+    Función que nos permitirá generar un digipymon aleatorio.
+    
+    Args:
+        vida (int): Genera puntos de vida aleatorios entre 10 y 20.
+        ataque (int): Genera puntos de ataque aleatorios entre 1 y 10.
+        nivel (int): Genera un nivel aleatorio entre 1 y 3.
+        tipo (str): Genera un tipo de digipymon entre fuego, agua y planta.
+        nombre (str): Genera un nombre aleatorio de digipymon de la lista de digipymons.
+    """
+    vida = random.randint(10, 20)
+    ataque = random.randint(1, 10)
+    nivel = random.randint(1,3)
+    tipo = random.choice("fuego", "agua", "planta")
+    nombre = ListaNombres.obtener_nombredigipymon()
+    
+    digipymonObjeto = Digipymon(nombre, tipo, nivel, vida, ataque)
+    return digipymonObjeto
 
 def menu_opciones():
     """
@@ -26,4 +47,8 @@ def menu_opciones():
 
     return seleccion
 
-
+def buscar_digipymon_aleatorio(jugador, inventario):
+    digipymonObjeto = generar_digipymon_aleatorio()
+    print(f"{digipymonObjeto}")
+    probabilidad = 100 - (digipymonObjeto.nivel * 10)
+    print(f"{probabilidad}")
